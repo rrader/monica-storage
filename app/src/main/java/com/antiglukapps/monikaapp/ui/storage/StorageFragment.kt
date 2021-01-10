2dc850e0f5bd445363d51f20450464a4d0c7d83c
Roman Rader <roman.rader@datarobot.com>
2021-01-10 22:37:41 +0200

    add items list

--- app/src/main/java/com/antiglukapps/monikaapp/ui/storage/StorageFragment.kt
+++ app/src/main/java/com/antiglukapps/monikaapp/ui/storage/StorageFragment.kt
@@ -5,32 +5,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.antiglukapps.monikaapp.AddStorageItemActivity
 import com.antiglukapps.monikaapp.R
+import com.antiglukapps.monikaapp.data.MonicaDatabase
+import com.antiglukapps.monikaapp.data.StorageItem
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-//@Serial
-//data class Task(var description: String, var completed: Boolean = false)
-
-class StorageFragment : Fragment() {
+class StorageFragment : Fragment(), StorageItemAdapter.OnStorageItemClickedListener {
 
     private lateinit var storageViewModel: StorageViewModel
+
+    private lateinit var storageRv: RecyclerView
+
+    private lateinit var monicaDatabase: MonicaDatabase
+    private lateinit var storageItemAdapter: StorageItemAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        monicaDatabase = MonicaDatabase.getInstance(requireActivity())
+        storageItemAdapter = StorageItemAdapter()
+        storageItemAdapter.setStorageItemClickedListener(this)
+
         storageViewModel =
             ViewModelProvider(this).get(StorageViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_storage, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_home)
-//        storageViewModel.text.observe(viewLifecycleOwner, Observer {
-//            textView.text = it
-//        })
+
+        storageRv = root.findViewById(R.id.storageRv)
 
         val add_button: FloatingActionButton = root.findViewById(R.id.floatingStorageAddButton)
         add_button.setOnClickListener {
@@ -41,5 +48,21 @@ class StorageFragment : Fragment() {
             )
         }
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        storageItemAdapter.storageItemsList = monicaDatabase.getStorageItemDao().getStorageItemList()
+        storageRv.adapter = storageItemAdapter
+        storageRv.layoutManager = LinearLayoutManager(activity)
+        storageRv.hasFixedSize()
+    }
+
+    override fun onStorageItemClicked(storageItem: StorageItem) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStorageItemLongClicked(storageItem: StorageItem) {
+        TODO("Not yet implemented")
     }
 }
